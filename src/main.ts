49cd90f6d3d95a4657eda168d4f4bf92f6c69307
Карачёв Игорь <karachevigor99@gmail.com>
2023-10-05 13:31:46 +0400

    add environments

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigDto } from './infrastructure/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    await AppModule.register(),
+  );
 
   const config = app.get(ConfigDto);
   await app.listen(config.http.port, config.http.host);
